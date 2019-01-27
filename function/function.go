35feef61f4477f382d3670d699d656957910efd2
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2019-01-27 22:55:04 +0900

    関数

---
+++ function/function.go
@@ -0,0 +1,21 @@
+package function
+
+import "math"
+
+// Loss function
+func Sigmoid(x float64) (y float64) {
+	return 1.0 / (1.0 + math.Exp(-1*x))
+}
+
+//内積計算
+func InnerProduct(w, x []float64) (f float64) {
+	if len(w) != len(x) {
+		panic("エラーですよ")
+	}
+
+	for i := range w {
+		f += w[i] * x[i]
+	}
+
+	return
+}
