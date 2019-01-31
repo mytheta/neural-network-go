659137c2f8c3cbd98c508457f4cd3ead869c1a34
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2019-01-31 15:49:04 +0900

    層

--- layer/middle.go
+++ layer/middle.go
@@ -1,11 +1,16 @@
 package layer
 
-import "github.com/mytheta/neural-network-go/function"
+import (
+	"fmt"
+	"math"
+
+	"github.com/mytheta/neural-network-go/function"
+)
 
 var (
-	wm1 = []float64{18.6, 11.1, 81.1}
-	wm2 = []float64{18.6, 11.1, 81.1}
-	wm3 = []float64{18.6, 11.1, 81.1}
+	wm1 = []float64{0.0, 1.09, 0.1}
+	wm2 = []float64{1.0, 1.5, 1.87}
+	wm3 = []float64{3.6, 2.03, 1.01}
 )
 
 func MiddleLayer1(x []float64) (y float64) {
@@ -16,45 +21,65 @@ func MiddleLayer1(x []float64) (y float64) {
 
 func MiddleLayer2(x []float64) (y float64) {
 	h := function.InnerProduct(wm2, x)
-	y = function.Sigmoid(h)
+	y = function.Round(function.Sigmoid(h), 8)
 	return
 }
 
 func MiddleLayer3(x []float64) (y float64) {
 	h := function.InnerProduct(wm3, x)
-	y = function.Sigmoid(h)
+	y = function.Round(function.Sigmoid(h), 8)
+
 	return
 }
 
-func MiddleErrorFunc1(e []float64, g float64) float64 {
-	h := function.InnerProduct(e, wo1)
-	return h * g * (1 - g)
-}
-
-func MiddleErrorFunc2(e []float64, g float64) float64 {
-	h := function.InnerProduct(e, wo2)
-	return h * g * (1 - g)
-}
-
-func MiddleErrorFunc3(e []float64, g float64) float64 {
-	h := function.InnerProduct(e, wo3)
-	return h * g * (1 - g)
+func MiddleErrorFunc(e []float64, g float64) float64 {
+	var h1, h2 float64
+	for _, epsilon := range e {
+		epsilon = function.Round(epsilon, 10)
+		h1 += function.Multiplication(epsilon, wo1)
+		h2 += function.Multiplication(epsilon, wo2)
+	}
+	r := (h1 + h2) * g
+	if math.IsNaN(r) {
+		panic("エラーですよ")
+	}
+	d := 1 - g
+	if math.IsNaN(d) {
+		panic("エラーですよ")
+	}
+	r = r * d
+	if math.IsNaN(r) {
+		panic("エラーですよ")
+	}
+	return r
 }
 
 func MiddleWeightCalc1(p, e, g float64) {
-	wm1[0] = wm1[0] - p*e*g
-	wm1[1] = wm1[1] - p*e*g
-	wm1[2] = wm1[2] - p*e*g
+	n := p * e * g
+	wm1[0] = wm1[0] - n
+	wm1[1] = wm1[1] - n
+	wm1[2] = wm1[2] - n
 }
 
 func MiddleWeightCalc2(p, e, g float64) {
-	wm2[0] = wm2[0] - p*e*g
-	wm2[1] = wm2[1] - p*e*g
-	wm2[2] = wm2[2] - p*e*g
+	n := p * e * g
+	wm2[0] = wm2[0] - n
+	wm2[1] = wm2[1] - n
+	wm2[2] = wm2[2] - n
 }
 
 func MiddleWeightCalc3(p, e, g float64) {
-	wm3[0] = wm3[0] - p*e*g
-	wm3[1] = wm3[1] - p*e*g
-	wm3[2] = wm3[2] - p*e*g
+	n := p * e * g
+	wm3[0] = wm3[0] - n
+	wm3[1] = wm3[1] - n
+	wm3[2] = wm3[2] - n
+}
+
+func PrintW() {
+	fmt.Println(wm1)
+	fmt.Println(wm2)
+	fmt.Println(wm3)
+	fmt.Println(wo1)
+	fmt.Println(wo2)
+
 }
--- layer/output.go
+++ layer/output.go
@@ -1,19 +1,23 @@
 package layer
 
-import "github.com/mytheta/neural-network-go/function"
-
-var (
-	wo1 = []float64{18.6, 11.1, 81.1}
-	wo2 = []float64{18.6, 11.1, 81.1}
+import (
+	"github.com/mytheta/neural-network-go/function"
 )
 
-func OutPutLayer1(x []float64) (h float64) {
-	h = function.InnerProduct(wo1, x)
+var (
+	wo1 = []float64{2.6, 1.1, 3.1}
+	wo2 = []float64{0.001, 0.07, 0.23}
+)
+
+func OutPutLayer1(x []float64) (y float64) {
+	h := function.InnerProduct(wo1, x)
+	y = function.Sigmoid(h)
 	return
 }
 
-func OutPutLayer2(x []float64) (h float64) {
-	h = function.InnerProduct(wo2, x)
+func OutPutLayer2(x []float64) (y float64) {
+	h := function.InnerProduct(wo2, x)
+	y = function.Sigmoid(h)
 	return
 }
 
@@ -22,13 +26,15 @@ func OutPutErrorFunc(g, b float64) float64 {
 }
 
 func OutWeightCalc1(p, e, g float64) {
-	wo1[0] = wo1[0] - p*e*g
-	wo1[1] = wo1[1] - p*e*g
-	wo1[2] = wo1[2] - p*e*g
+	n := p * e * g
+	wo1[0] = wo1[0] - n
+	wo1[1] = wo1[1] - n
+	wo1[2] = wo1[2] - n
 }
 
 func OutWeightCalc2(p, e, g float64) {
-	wo2[0] = wo2[0] - p*e*g
-	wo2[1] = wo2[1] - p*e*g
-	wo2[2] = wo2[2] - p*e*g
+	n := p * e * g
+	wo2[0] = wo2[0] - n
+	wo2[1] = wo2[1] - n
+	wo2[2] = wo2[2] - n
 }
